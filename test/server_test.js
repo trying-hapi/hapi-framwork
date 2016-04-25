b3837b12539a0d74f003d89816700251e658c6d7
Katherine Beame <katherine.beame@gmail.com>
2016-04-25 12:15:47 -0700

    linted all the files for turn in

--- test/server_test.js
+++ test/server_test.js
@@ -4,91 +4,93 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const request = chai.request;
 const mongoose = require('mongoose');
-const port = 3000;
 process.env.MONGODB_URI = 'mongodb://localhost/planet_test_db';
 require(__dirname + '/../server');
 const Planet = require(__dirname + '/../model/model');
 
-describe('The POST method', ()=>{
-  after((done)=>{
-    mongoose.connection.db.dropDatabase(()=>{
+describe('The POST method', () => {
+  after((done) => {
+    mongoose.connection.db.dropDatabase(() => {
       done();
     });
   });
 
-it('should make a solar system', (done)=>{
-  request('localhost:3000')
-  .post('/planets')
-  .send({name:'Qo\'noS', color: 'green', size:'earth-like', moonsNumber:0})
-  .end((err, res)=>{
-    expect(err).to.eql(null);
-    expect(res.body.color).to.eql('green');
-    expect(res.body.name).to.eql('Qo\'noS');
-    expect(res.body.size).to.eql('earth-like');
-    expect(res.body.moonsNumber).to.eql(0);
-    done();
+  it('should make a solar system', (done) => {
+    request('localhost:3000')
+    .post('/planets')
+    .send({ name: 'Qo\'noS', color: 'green', size: 'earth-like', moonsNumber: 0 })
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res.body.color).to.eql('green');
+      expect(res.body.name).to.eql('Qo\'noS');
+      expect(res.body.size).to.eql('earth-like');
+      expect(res.body.moonsNumber).to.eql(0);
+      done();
+    });
   });
 });
-});
 
-describe('the GET', (done)=>{
-it('should get the planets', (done)=>{
-  request('localhost:3000')
-  .get('/planets')
-  .end((err, res)=>{
-    expect(err).to.eql(null);
-    expect(Array.isArray(res.body)).to.eql(true);
-    expect(res.body.length).to.eql(0);
-    done();
+describe('the GET', () => {
+
+  it('should get the planets', (done) => {
+    request('localhost:3000')
+    .get('/planets')
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(Array.isArray(res.body)).to.eql(true);
+      expect(res.body.length).to.eql(0);
+      done();
+    });
   });
 });
-});
 
 
-describe('routes that need a planet in the db', ()=>{
-  beforeEach((done)=>{
-    var newPlanet = new Planet({name:'Vulcan', color: 'greenish', size:'earth-like', moonsNumber:0})
-    newPlanet.save((err, data)=>{
+describe('routes that need a planet in the db', () => {
+  beforeEach((done) => {
+    var newPlanet = new Planet({ name: 'Vulcan', color: 'greenish', size: 'earth-like', moonsNumber: 0 });
+    newPlanet.save((err, data) => {
+      if (err) throw err;
       this.planet = data;
-          done();
+      done();
     });
   });
 
 
-afterEach((done)=>{
-this.planet.remove((err)=>{
-    done();
+  afterEach((done) => {
+    this.planet.remove((err) => {
+      if (err) throw err;
+      done();
+    });
   });
-});
-//
-after((done)=>{
-  mongoose.connection.db.dropDatabase(()=>{
-    done();
-  });
-});
 
-it('should the Put', (done)=>{
-  request('localhost:3000')
-  .put('/planets/' + this.planet.name)
-  .send({name:'Vulcan', color: 'greenish1', size:'earth-like1', moonsNumber:0})
-  .end((err, res)=>{
-    expect(err).to.eql(null);
-    console.log(err);
-    expect(res.body.color).to.eql('greenish1');
-    expect(res.body.size).to.eql('earth-like1');
-    expect(res.body.moonsNumber).to.eql(0);
-    done();
+  after((done) => {
+    mongoose.connection.db.dropDatabase(() => {
+      done();
+    });
   });
-});
+
+  it('should the Put', (done) => {
+    request('localhost:3000')
+    .put('/planets/' + this.planet.name)
+    .send({ name: 'Vulcan', color: 'greenish1', size: 'earth-like1', moonsNumber: 0 })
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      console.log(err);
+      expect(res.body.color).to.eql('greenish1');
+      expect(res.body.size).to.eql('earth-like1');
+      expect(res.body.moonsNumber).to.eql(0);
+      done();
+    });
+  });
 
 
-it('should DELETE', (done)=>{
-  request('localhost:3000')
-  .delete('/planets/' + this.planet.name)
-  .end((err, res)=>{
-    expect(err).to.eql(null);
-    expect(res.body.message).to.eql('OMG! We destroyed a planet!');
-    done();
+  it('should DELETE', (done) => {
+    request('localhost:3000')
+    .delete('/planets/' + this.planet.name)
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res.body.message).to.eql('OMG! We destroyed a planet!');
+      done();
+    });
   });
-});
 });
